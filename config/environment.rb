9ff3875e81c44ed37e5f45529ad603e77c29a27d
Marcela Porto <email": null>
2017-03-17 19:33:44 -0700

    Requires iso_country_codes

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ require "sinatra/reloader" if development?
 enable :sessions
 
 require 'erb'
+require 'iso_country_codes'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
