915127519701a557c9ea5b7f9448666d87f27920
Marcela Porto <email": null>
2017-03-20 22:09:59 -0700

    Adds small comments

--- db/migrate/20170317132427_create_preferences.rb
+++ db/migrate/20170317132427_create_preferences.rb
@@ -1,7 +1,7 @@
 class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
-      t.references :users
+      t.references :users #not working
       t.references :preferable, polymorphic: true, index: true
     end
   end
