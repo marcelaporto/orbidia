7d640454a996b6d10a219e2c14d9088a1087b814
Marcela Porto <email": null>
2017-03-20 21:46:13 -0700

    Adds countries_languages gem

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ require 'erb'
 # Add ons for the app
 require 'iso_country_codes'
 require 'faker'
+require 'language_list'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
