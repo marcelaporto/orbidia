d5c1907728aa50bebce599c09da4b1517d0fa700
Marcela Porto <email": null>
2017-03-21 20:27:36 -0700

    Adds new user (still no preference added)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,18 @@
 get '/register' do
-  @countries = Country.all
+  @countries = Country.order(:name)
   @languages = Language.order(:name)
   @moods = Mood.all
   erb :'user/new'
 end
 
 post '/register' do
-# FIRST DRAFT OF REGISTER
-# @user = User.new(params[:user])
+@user = User.new(full_name: params[:user][:full_name], email: params[:user][:email], password: params[:user][:password], country: Country.find_by(name: params[:user][:country]))
+p "*" * 30
+p @user
+p params[:preference]
+p "*" * 30
 # params[:preferable].each_with_index do |index, thing|
-#   Preference.new(user: @user, preferable: thing)
+# Preference.new(user: @user, preferable: thing)
 # end
 end
 
