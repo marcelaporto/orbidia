32ad1518f67aec31bf01e04020f94f6f288711f3
Marcela Porto <email": null>
2017-03-23 14:57:47 -0700

    Adds gravatar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,16 @@ end
 post '/register' do
   @user = User.new(full_name: params[:user][:full_name], email: params[:user][:email], password: params[:user][:password], country: Country.find_by(name: params[:user][:country]))
   login(@user)
+
+  @image = @user.get_gravatar(@user.email)
   redirect "/user/#{@user.id}"
 end
 
 post '/login' do
   @user = User.authenticate(params[:email], params[:password])
   if @user
-    login
+    login(@user)
+    @image = @user.get_gravatar(@user.email)
     redirect "/user/#{@user.id}"
   else
     # @error
