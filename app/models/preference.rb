b11d488def2861bb90a494e67f9665d17a97240a
Marcela Porto <email": null>
2017-03-23 14:40:25 -0700

    Deletes models and controllers for MVP

--- app/models/preference.rb
+++
@@ -1,7 +0,0 @@
-class Preference < ActiveRecord::Base
-  belongs_to :user
-  has_many :countries, as: :preferable
-  has_many :languages, as: :preferable
-  has_many :genres, as: :preferable
-  has_many :mood, as: :preferable
-end
