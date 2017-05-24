6e5d223bb8127ec3177a6d5f5f4ce6f880d13b3d
Marcela Porto <marcela.fporto@gmail.com>
2017-05-23 18:58:29 -0700

    Creates rails app w/ database

---
+++ orbidia-rails/db/migrate/20170524015713_create_country_languages.rb
@@ -0,0 +1,8 @@
+class CreateCountryLanguages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :country_languages do |t|
+      t.references :country, foreign_key: true
+      t.references :language, foreign_key: true
+    end
+  end
+end
