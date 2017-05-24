3960d858dbbb70495674b6ca3b793e1a8fdfb472
Marcela Porto <marcela.fporto@gmail.com>
2017-05-23 21:00:51 -0700

    Adds sessions & users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/signup' => 'users#new'
+  get '/users/:id' => 'users#show'
+  post '/users' => 'users#create'
+
+  get '/' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
